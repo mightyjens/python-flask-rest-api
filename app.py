2be2622eecdbc029e323f2069d98bdac32269d8d
Jens Fritzsche <me@mightyjens.de>
2021-05-20 09:02:07 +0200

    Verschiedene

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-from api.v1.routes import api as api_v1
+
+# Define routes
+# https://realpython.com/flask-blueprint/#what-a-flask-application-looks-like
+from v1.routes import api as api_v1
 
 app = Flask(__name__)
 app.register_blueprint(api_v1, url_prefix='/v1')
--- v1/routes.py
+++ v1/routes.py
@@ -1,7 +1,9 @@
+import common.functions
+
 from flask import Blueprint
 
 api = Blueprint('api_v1', __name__)
 
 @api.route('/')
 def index():
-    return "This is an example app"
+    return common.functions.helloworld
